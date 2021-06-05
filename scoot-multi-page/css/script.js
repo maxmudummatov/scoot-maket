ba9ce483a25a6be50c3bb9c62afa8482f66bca6f
maxmudummatov <82269989+maxmudummatov@users.noreply.github.com>
2021-06-05 21:36:56 +0500

    changed

---
+++ scoot-multi-page/css/script.js
@@ -0,0 +1,23 @@
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('menu');
+let toggle = false;
+
+// Functions
+
+function toggleIcon(){
+
+    if (toggle){
+        navToggle.style.backgroundImage = 'url(../../assets/icons/hamburger.svg)';
+    } else {
+        navToggle.style.backgroundImage = 'url(../../assets/icons/close.svg)';
+    }
+
+    toggle = !toggle;
+}
+
+// Main
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('showMenu');
+
+    toggleIcon();
+});
